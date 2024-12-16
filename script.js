4f90b90b5fa2100b9a7ea68d1cf693d1dbb2703d
Balazs <121223458+H4RmYxDD@users.noreply.github.com>
2024-12-16 13:16:44 +0100

    lyo

--- script.js
+++ script.js
@@ -7,12 +7,19 @@ async function getJoke() {
   try {
     const response = await fetch(apiURL);
     const data = await response.json();
+    jokeDisplay.style.opacity = 0;
+    jokeDisplay.style.transform = 'translateY(20px)';
 
-    if (data.type === 'twopart') {
-      jokeDisplay.innerHTML = `<strong>${data.setup}</strong><br><em>${data.delivery}</em>`;
-    } else {
-      jokeDisplay.innerHTML = `<strong>${data.joke}</strong>`;
-    }
+    setTimeout(() => {
+      if (data.type === 'twopart') {
+        jokeDisplay.innerHTML = `<strong>${data.setup}</strong><br><em>${data.delivery}</em>`;
+      } else {
+        jokeDisplay.innerHTML = `<strong>${data.joke}</strong>`;
+      }
+
+      jokeDisplay.style.opacity = 1;
+      jokeDisplay.style.transform = 'translateY(0)';
+    }, 300);
   } catch (error) {
     jokeDisplay.innerHTML = "Hiba történt a vicc betöltése közben!";
   }
